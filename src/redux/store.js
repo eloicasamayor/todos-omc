a225468bdb6bbe7e2ffddf5ac40ca1a7e357f706
Eloi <eloi.casamayor@gmail.com>
2022-03-29 16:53:54 +0200

    redux-devtools installed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { reduceFilters } from "./filters/reducers";
 import { reduceTodos, todosMiddleware } from "./todos";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducer = combineReducers({ todos: reduceTodos, filters: reduceFilters });
 
 export const store = createStore(
   reducer,
-  compose(
-    applyMiddleware(todosMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeWithDevTools(applyMiddleware(todosMiddleware))
 );
