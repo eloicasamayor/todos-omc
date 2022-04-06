65b06fc2c10d9caca12bd461323d7a26d10b2ee5
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2022-04-06 17:13:47 +0200

    login page (not working yet)

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -3,8 +3,14 @@ import { Outlet } from "react-router-dom";
 export function Layout() {
   return (
     <div>
-      <h1>Todo list</h1>
+      <header>
+        <h1>Todo list</h1>
+      </header>
+
       <Outlet />
+      <footer className="footer mt-auto py-3 bg-light">
+        <p>Developed by Eloi Casamayor Esteve</p>
+      </footer>
     </div>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,26 @@
 export function Login() {
   return (
-    <div>
+    <div className="Card mx-auto w-50">
       <h1>Login</h1>
+      <form>
+        <div class="form-outline mb-4">
+          <input type="text" id="inputUser" class="form-control" />
+          <label class="form-label" for="inputUser">
+            User
+          </label>
+        </div>
+
+        <div class="form-outline mb-4">
+          <input type="password" id="inputPassword" class="form-control" />
+          <label class="form-label" for="inputPassword">
+            Password
+          </label>
+        </div>
+
+        <button type="button" class="btn btn-primary btn-block mb-4">
+          Sign in
+        </button>
+      </form>
     </div>
   );
 }
